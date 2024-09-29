c549f916e37eb024435fac2c77b3d32163ca4d16
thermoNuke1 <ca372717@gmail.com>
2024-09-29 13:20:16 -0400

    Setting up database connection

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,21 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors =  require('cors')
-
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+
+
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to', config.MONGODB_URL)
+
+mongoose.connect(config.MONGODB_URL).then(result =>{
+    logger.info('connected to MongoDB')
+}).catch(error => {
+    logger.error('error connecting to MongoDB', error.message)
+})
 
 
 app.use(cors())
--- backend/utils/config.js
+++ backend/utils/config.js
@@ -1,6 +1,11 @@
 require('dotenv').config()
+
 const PORT = process.env.PORT
+const MONGODB_URL = process.env.NODE_ENV === 'test'
+ ? process.env.TEST_MONGODB_URL
+ : process.env.MONGODB_URL
 module.exports = {
+    MONGODB_URL,
     PORT
 }
 // this file is for configurations and possibly switching between a test env and a prod env
