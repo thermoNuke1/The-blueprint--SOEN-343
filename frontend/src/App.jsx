59006bf7420e2b854b1b121ce16dbdce8691ae7d
atsatas <a_tsatas@live.concordia.ca>
2024-09-30 11:28:49 -0400

    NavBar started, CSS needs to be done to make it like the Figma prototype

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
+import { useState } from "react";
+import Navbar from "./Navbar";
 
 const App = () => {
+	return (
+		<div>
+			<Navbar />
 
-    return ( 
-    <div>
-      <h1>GOATS</h1>
- 
-    </div>
-    )
-  }
-  
-  export default App
-  
+			<h1>GOATS</h1>
+		</div>
+	);
+};
+
+export default App;
