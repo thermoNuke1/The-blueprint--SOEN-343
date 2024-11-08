aacfa075c7482d1921d827b636caae980c485cfe
ThermoNuke1 <ca372717@gmail.com>
2024-11-08 10:26:41 -0500

    build:ui

--- frontend/src/services/payment.js
+++ frontend/src/services/payment.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000'; // URL of the backend
+const API_URL = '/api/payment';
 
 export const createPaymentIntent = async (amount, paymentMethodId) => {
   try {
-    const response = await axios.post(`${API_URL}/create-payment-intent`, {
+    const response = await axios.post(`${API_URL}`, {
       amount,
       paymentMethodId,
     });
