9785c970bc3bf6c2b2b998923b3b947f50c61c96
sydneyC678 <sydcampbell@hotmail.com>
2024-11-09 20:17:25 -0500

    app.jsx testing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,24 +10,31 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import CheckoutForm from "./components/checkoutForm.jsx";
 import { Elements} from '@stripe/react-stripe-js';
 import { loadStripe } from '@stripe/stripe-js';
+import ShipmentProgressBar from "./components/Tracking/shipmentProgressBar.jsx";
+import Tracking from "./pages/TrackingPage.jsx";
 
 const stripePromise = loadStripe('pk_test_51QIZoARrCeYLfUcjF4kwH421Z5YCAybTbMhfwQKW2jCH0yRAOzy3Bqdu2BM021tNJLdyfX3txaqNGSLnxXZBS0Xq00lXkPvRFa');
 
 const App = () => {
 	// const [errorMessage, setErrorMessage] = useState(null);
 	// const [user, setUser] = useState(null);
+
 	return (
-		<div>
-			<Homepage></Homepage>
+		<>
+			{/* <Homepage /> */} 
+			
 			{/* <Navbar/> */}
 			{/* <Notification message={errorMessage} /> */}
 			{/* <Login setErrorMessage={setErrorMessage} user={user} setUser={setUser} /> */}
+			
+			<Tracking></Tracking>
 			<Elements stripe={stripePromise}>
 				<CheckoutForm />
 			</Elements>
+		</>
 
 			
-		</div>
+		
 	);
 };
 
